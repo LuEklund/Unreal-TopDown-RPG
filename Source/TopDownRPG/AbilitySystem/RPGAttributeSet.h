bcfeb9b8e7905a8e71253ead11788027e1a2a748
LuEklund <luckeeklund@gmail.com>
2024-04-28 18:57:41 +0300

    Effect Actor working with curve now

--- Source/TopDownRPG/AbilitySystem/RPGAttributeSet.h
+++ Source/TopDownRPG/AbilitySystem/RPGAttributeSet.h
@@ -13,6 +13,39 @@
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+	FEffectProperties(){}
+
+	FGameplayEffectContextHandle EffectContextHandle;
+	UPROPERTY()
+	UAbilitySystemComponent	*SourceASC = nullptr;
+
+	UPROPERTY()
+	AActor					*SourceAvatarActor = nullptr;
+
+	UPROPERTY()
+	AController				*SourceController = nullptr;
+
+	UPROPERTY()
+	ACharacter				*SourceCharacter = nullptr;
+
+	UPROPERTY()
+	UAbilitySystemComponent	*TargetASC = nullptr;
+
+	UPROPERTY()
+	AActor					*TargetAvatarActor = nullptr;
+
+	UPROPERTY()
+	AController				*TargetController = nullptr;
+
+	UPROPERTY()
+	ACharacter				*TargetCharacter = nullptr;
+};
+
 /**
  * 
  */
@@ -20,9 +53,14 @@ UCLASS()
 class TOPDOWNRPG_API URPGAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+private:
+	void	SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties &Props) const;
 public:
 	URPGAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void	PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void	PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
 	FGameplayAttributeData	Health;
--- Source/TopDownRPG/Actor/RPGEffectActor.cpp
+++ Source/TopDownRPG/Actor/RPGEffectActor.cpp
@@ -30,8 +30,66 @@ void ARPGEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayE
 	check(GameplayEffectClass);
 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
-	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, ActorLevel, EffectContextHandle);
+	FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+
+	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite && InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		ActiveEffectHandles.Add(ActiveEffectHandle, TargetASC);
+	}
+}
+
+void ARPGEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if (InfiniteEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+
+}
+
+void ARPGEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if (DurationEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if (InfiniteEffectApplicationPolicy ==EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+	if (InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent *TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if (!IsValid(TargetActor)) return ;
+
+		TArray<FActiveGameplayEffectHandle>	HandleToRemove;
+		for (TTuple<FActiveGameplayEffectHandle, UAbilitySystemComponent*> HandlePair : ActiveEffectHandles)
+		{
+			if (TargetASC == HandlePair.Value)
+			{
+				TargetASC->RemoveActiveGameplayEffect(HandlePair.Key, 1);
+				HandleToRemove.Add(HandlePair.Key);
+			}
+		}
+		for (FActiveGameplayEffectHandle &Handle : HandleToRemove)
+		{
+			ActiveEffectHandles.FindAndRemoveChecked(Handle);
+		}
+	}
 }
 
 
