40be037d83cd630465568f550c8e8e8ccb51af36
LuEklund <luckeeklund@gmail.com>
2024-05-04 00:06:52 +0300

    Crazzy Ai finding you both Meele and Ranged, they canta cttack yet but they do draw a red debusgspehre for attacking

---
+++ Source/TopDownRPG/AI/BTTask_Attack.cpp
@@ -0,0 +1,10 @@
+
+
+
+#include "BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+	
+}
