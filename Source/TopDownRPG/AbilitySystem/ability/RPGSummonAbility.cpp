cade75372f4026805f7819b012a775af7181b57d
LuEklund <luckeeklund@gmail.com>
2024-05-09 17:56:07 +0300

    now it summon stuff

--- Source/TopDownRPG/AbilitySystem/ability/RPGSummonAbility.cpp
+++ Source/TopDownRPG/AbilitySystem/ability/RPGSummonAbility.cpp
@@ -3,14 +3,11 @@
 
 #include "RPGSummonAbility.h"
 
-#include "Kismet/KismetSystemLibrary.h"
-
 TArray<FVector> URPGSummonAbility::GetSpawnLocations()
 {
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
 	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
 	const float DeltaSpread = SpawnSpread/NumMinions;
-	const FVector RightOfSpread = Forward.RotateAngleAxis(SpawnSpread / 2.f, FVector::UpVector);
 	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
 
 	TArray<FVector>	SpawnLocations;
@@ -18,14 +15,22 @@ TArray<FVector> URPGSummonAbility::GetSpawnLocations()
 	for (int32 i = 0; i < NumMinions; ++i)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
-		const FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
-		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false,3.f);
-		SpawnLocations.Add(ChosenSpawnLocation);
+		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f,  0.f, 400.f),ChosenSpawnLocation + FVector(0.f, 0.f, -400.f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			ChosenSpawnLocation = Hit.ImpactPoint;
+		}
 		
-		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Green, 3.f);
-		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false,3.f);
-		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false,3.f);
-	
+		SpawnLocations.Add(ChosenSpawnLocation);
 	}
 	return SpawnLocations;
+}
+
+TSubclassOf<APawn> URPGSummonAbility::GetRandomMinionClass()
+{
+	int32 Selection = FMath::RandRange(0, MinionClasses.Num() - 1);
+	return MinionClasses[Selection];
 }
