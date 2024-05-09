4b08c5aa4e3814b0b426793e1fae0eca5b7ac145
LuEklund <luckeeklund@gmail.com>
2024-05-09 17:21:38 +0300

    add spawning ability

---
+++ Source/TopDownRPG/AbilitySystem/ability/RPGSummonAbility.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RPGGameplayAbility.h"
+#include "RPGSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNRPG_API URPGSummonAbility : public URPGGameplayAbility
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector>	GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	int32	NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float	MinSpawnDistance = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float	MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float SpawnSpread = 90.f;
+};
