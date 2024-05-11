03f3b58fd4c1208d7b629e96d5facb107418b9b7
LuEklund <luckeeklund@gmail.com>
2024-05-11 12:27:06 +0300

    level data asset added

---
+++ Source/TopDownRPG/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		//LevelUpInformation[1] = Level 1 Information
+		//LevelUpInformation[2] = Level 1 Information
+		//Because start counting levels from level 0
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+
+	}
+	return Level;
+}
