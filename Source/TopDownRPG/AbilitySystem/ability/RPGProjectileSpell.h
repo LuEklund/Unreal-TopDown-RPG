efc423df0714fc0042c1cd65a6348dc128edb4be
LuEklund <luckeeklund@gmail.com>
2024-05-17 07:58:43 +0300

    updated text for firebolt

--- Source/TopDownRPG/AbilitySystem/ability/RPGProjectileSpell.h
+++ Source/TopDownRPG/AbilitySystem/ability/RPGProjectileSpell.h
@@ -14,6 +14,9 @@ UCLASS()
 class TOPDOWNRPG_API URPGProjectileSpell : public URPGDamageGameplayAbility
 {
 	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString	GetNextLevelDescription(int32 Level) override;
 
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
@@ -23,5 +26,8 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<ARPGProjectile>	ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32	NumProjectiles = 5;
 
 };
